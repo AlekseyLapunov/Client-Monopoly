8892f79fa35e6bbcd9a8573f3e17ba3a34d721b0
Aleksei Lapunov <lapunoff.alex@yandex.ru>
2023-03-14 02:40:54 +0700

    finished LobbyWindow UI, made slots for its UI elements. global change: replaced Cambria font with Bookman Old Style due to cross-platform need

--- userinterface/lobbywindow.cpp
+++ userinterface/lobbywindow.cpp
@@ -12,3 +12,38 @@ LobbyWindow::~LobbyWindow()
 {
     delete ui;
 }
+
+void LobbyWindow::toggleLobbyVision()
+{
+
+}
+
+void LobbyWindow::startGame()
+{
+
+}
+
+void LobbyWindow::setLobbySettings()
+{
+
+}
+
+void LobbyWindow::setGameSettings()
+{
+
+}
+
+void LobbyWindow::leaveLobby()
+{
+
+}
+
+void LobbyWindow::toggleReadyStatus()
+{
+
+}
+
+void LobbyWindow::quitApp()
+{
+    QCoreApplication::quit();
+}
--- userinterface/lobbywindow.h
+++ userinterface/lobbywindow.h
@@ -2,18 +2,31 @@
 #define LOBBYWINDOW_H
 
 #include <QMainWindow>
+#include <QCoreApplication>
+
+#include "helpers/basewin.h"
 
 namespace Ui {
 class LobbyWindow;
 }
 
-class LobbyWindow : public QMainWindow
+class LobbyWindow : public QMainWindow, public BaseWin
 {
     Q_OBJECT
 
 public:
     explicit LobbyWindow(QWidget *parent = nullptr);
     ~LobbyWindow();
+
+private slots:
+    void toggleLobbyVision();
+    void startGame();
+    void setLobbySettings();
+    void setGameSettings();
+    void leaveLobby();
+    void toggleReadyStatus();
+    void quitApp();
+
 
 private:
     Ui::LobbyWindow *ui;
